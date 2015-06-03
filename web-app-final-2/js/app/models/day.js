580c2b15c4672f9ba1e6997a9d2e0e0184f0fde9
Brandon Blackmon <plusbrandon@gmail.com>
2015-06-03 14:53:50 -0700

    Add the ability to add new cities

---
+++ web-app-final-2/js/app/models/day.js
@@ -0,0 +1,33 @@
+define([
+	"backbone"
+], function (Backbone) {
+
+	'use strict';
+
+	var DayModel = Backbone.Model.extend({
+		defaults: {
+			'weekday': '',
+			'conditions': '',
+			'highCelsius': null,
+			'lowCelsius': null,
+			'highFahrenheit': null,
+			'lowFahrenheit': null,
+			'icon_url': ''
+		},
+		parse: function(data) {
+			var map = {
+				'weekday': data.date.weekday,
+				'conditions': data.conditions,
+				'highCelsius': data.high.celsius,
+				'lowCelsius': data.low.celsius,
+				'highFahrenheit': data.high.fahrenheit,
+				'lowFahrenheit': data.low.fahrenheit,
+				'icon_url': data.icon_url
+			};
+			return map;
+		}
+	});
+
+	return DayModel;
+
+});
